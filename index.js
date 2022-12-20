ee4cdadd4f283975940baed9fe3a108ebe2f1d64
BillStephens2022 <two4onebill@yahoo.com>
2022-12-20 16:44:49 -0500

    added functions for prompting to add additional team members and adding engineers and interns

--- index.js
+++ index.js
@@ -2,48 +2,140 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-//Import classes
+//Import various Employee classes
 const Employee = require('./lib/employee');
 const Manager = require('./lib/manager');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
 
+let addEmployeeBoolean = true;
+
 /* Create a team array that will hold all members of the team and related property values.
 Will ultimately be used to render cards in an HTML file.*/
 
-teamArray = [];
+const teamArray = [];
 
-/* function allowing user to create a manager by answering questions in the command
+/* function for user to create a manager by answering questions in the command
 line using the Node Inquirer package.  Manager will then be added to the team array. */
 function addManager() {
-    inquirer
+    return inquirer
         .prompt ([
             {
                 type: "input",
                 name: "name",
-                message: "What is the manager's name for this team?",
+                message: "What is the team manager's name?",
             },
             {
                 type: "input",
                 name: "id",
-                message: "What is the manager's employee ID?",
+                message: "What is the team manager's employee ID?",
             },
             {
                 type: "input",
                 name: "email",
-                message: "What is the manager's email?",
+                message: "What is the team manager's email?",
             },
             {
                 type: "input",
                 name: "officeNumber",
-                message: "what is the manager's office number?",
-            }
+                message: "what is the team manager's office number?",
+            },
+          
         ])
         .then(managerData => {
             const manager = new Manager(managerData.name, managerData.id, managerData.email, managerData.officeNumber);
             teamArray.push(manager);
-            console.log(manager);
-        })
+            addEmployeePrompt();
+        });
 }
 
-addManager();
+function addEmployeePrompt() {
+    return inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "employeeType",
+                message: "What type of employee would you like to add to your team?",
+                choices: ["Engineer", "Intern", "No other team members to add"],
+            },
+        ])
+        .then(function(employeeChoice){
+            if (employeeChoice.employeeType === "Engineer") {
+                addEngineer();
+            }
+            else if (employeeChoice.employeeType === "Intern") {
+                addIntern();  
+            } else {
+                addEmployeeBoolean = false;   //will possibly change to generateHTML() later
+            } 
+        });
+}
+
+function addEngineer() {
+    return inquirer
+        .prompt ([
+            {
+                type: "input",
+                name: "name",
+                message: "What is the Engineer's name?",
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is the Engineer's employee ID?",
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is the Engineer's email?",
+            },
+            {
+                type: "input",
+                name: "gitHub",
+                message: "what is the Engineer's GitHub profile?",
+            },
+        ])
+        .then(engineerData => {
+            const engineer = new Engineer(engineerData.name, engineerData.id, engineerData.email, engineerData.gitHub);
+            teamArray.push(engineer);
+            addEmployeePrompt();
+        });
+}
+
+function addIntern() {
+    return inquirer
+        .prompt ([
+            {
+                type: "input",
+                name: "name",
+                message: "What is the Intern's name?",
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is the Intern's employee ID?",
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is the Intern's email?",
+            },
+            {
+                type: "input",
+                name: "school",
+                message: "what is the Intern's school?",
+            },
+        ])
+        .then(internData => {
+            const intern = new Intern(internData.name, internData.id, internData.email, internData.school);
+            teamArray.push(intern);
+            addEmployeePrompt();
+        });
+}
+
+
+addManager();  //add Manager kicks off entire series of prompts including adding other employees
+
+
+ 
+
